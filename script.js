59827dbda504ece9b968d6deaef4ac0a0bbd3a48
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-05-18 17:39:31 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://jet-wishlist-hypothesis-elvis.trycloudflare.com"; 
+const BASE_URL = "https://bay-tanzania-mass-him.trycloudflare.com"; 
 const API_URL = ${BASE_URL}/latest; // สำหรับโหลดข้อมูลเซ็นเซอร์
 const ASK_AI_URL = ${BASE_URL}/ask-ai; // สำหรับถาม AI
 
