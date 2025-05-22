85a9d4032eb2425c28dca4b501f55b9c9b789a55
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-05-22 20:45:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://action-determined-draws-stars.trycloudflare.com";
+const BASE_URL = "https://biwbong.loca.lt";
 const API_URL = `${BASE_URL}/latest`;      // โหลดข้อมูลเซ็นเซอร์
 const ASK_AI_URL = `${BASE_URL}/ask-ai`;   // ถาม AI
 
