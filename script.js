d254d4a35658cb7b249eb61cd50f849d69ad8dde
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-05-22 22:22:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://biwbong.loca.lt";
+const BASE_URL = "https://quizzes-tue-bangladesh-generate.trycloudflare.com";
 const API_URL = `${BASE_URL}/latest`;      // โหลดข้อมูลเซ็นเซอร์
 const ASK_AI_URL = `${BASE_URL}/ask-ai`;   // ถาม AI
 
