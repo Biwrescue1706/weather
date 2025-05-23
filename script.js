ff01ebdb96e513d06f4b3b4b82017052ec4ff1ba
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-05-23 10:21:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://disclaimer-telephone-celtic-crm.trycloudflare.com";
+const BASE_URL = "https://jennifer-acne-obviously-renew.trycloudflare.com";
 const API_URL = `${BASE_URL}/latest`;      // โหลดข้อมูลเซ็นเซอร์
 const ASK_AI_URL = `${BASE_URL}/ask-ai`;   // ถาม AI
 
