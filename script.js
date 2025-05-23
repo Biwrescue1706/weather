87439dfba781450673c911499580a898443ace69
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-05-23 11:55:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://jennifer-acne-obviously-renew.trycloudflare.com";
+const BASE_URL = "https://stephen-remember-rca-carefully.trycloudflare.com";
 const API_URL = `${BASE_URL}/latest`;      // โหลดข้อมูลเซ็นเซอร์
 const ASK_AI_URL = `${BASE_URL}/ask-ai`;   // ถาม AI
 
