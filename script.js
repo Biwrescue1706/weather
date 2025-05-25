fee1c2805c3cb8f253ffc9b094f8cfe0672fa4c5
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-05-25 23:23:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://efforts-durham-cited-tri.trycloudflare.com";
+const BASE_URL = "https://lottery-rare-nutten-western.trycloudflare.com";
 const API_URL = `${BASE_URL}/latest`;      // โหลดข้อมูลเซ็นเซอร์
 const ASK_AI_URL = `${BASE_URL}/ask-ai`;   // ถาม AI
 
